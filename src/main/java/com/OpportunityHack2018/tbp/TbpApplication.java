3af463866b71f6b87f6d3a79503b3da3c0e000ca
vivek <vivek.d.agarwal@sjsu.edu>
2017-10-28 14:50:44 -0700

    Initial commit

---
+++ src/main/java/com/OpportunityHack2018/tbp/TbpApplication.java
@@ -0,0 +1,12 @@
+package com.OpportunityHack2018.tbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbpApplication.class, args);
+	}
+}
