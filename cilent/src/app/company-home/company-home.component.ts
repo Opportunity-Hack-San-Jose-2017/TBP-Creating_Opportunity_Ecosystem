3d736673feee9f0760ac930288cd451f2e09a732
Sheldon Bazzell <sheldon.bazzell@gmail.com>
2017-10-29 11:52:23 -0700

    company log/reg styles

--- cilent/src/app/company-home/company-home.component.ts
+++ cilent/src/app/company-home/company-home.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-company-home',
   templateUrl: './company-home.component.html',
   styleUrls: ['./company-home.component.css']
 })
-export class CompanyHomeComponent implements OnInit {
+export class CompanyHomeComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+	login: Boolean = true;
+  
+  handleClick(id: String) {
+    this.login = id === 'login' ? true : false;
   }
 
 }
