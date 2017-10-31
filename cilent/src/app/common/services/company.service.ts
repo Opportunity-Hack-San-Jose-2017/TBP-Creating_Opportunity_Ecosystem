ee0831d7238264b8029fcde49a32684a9fa10420
Sheldon Bazzell <sheldon.bazzell@gmail.com>
2017-10-30 21:33:17 -0700

    added typecasting, cleaned formatting in services

--- cilent/src/app/common/services/company.service.ts
+++ cilent/src/app/common/services/company.service.ts
@@ -1,40 +1,42 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
-
 
 @Injectable()
 export class CompanyService {
 
-  constructor(private http: HttpClient, private _router: Router) { }
+	constructor(
+		private http: HttpClient,
+		private _router: Router
+	) { }
 
-  getCompanyProfile(){
-  	this.http.get('http://54.183.64.109/company/profile')
-    .subscribe(data => console.log(data))
-  }
+	getCompanyProfile(){
+		this.http.get('http://54.183.64.109/company/profile')
+			.subscribe((data: any) => console.log(data))
+	}
 
-  register(data: Object){
+	register(data: Object){
+		this.http.post('http://54.183.64.109/company/register', data)
+			.subscribe(
+				(data: any) => {
+					this._router.navigate(['/company/opening/create'])
+				}, (error: HttpErrorResponse) => {
+					console.log(error)
+				})
+	}
 
-  	this.http.post('http://54.183.64.109/company/register', data)
-    .subscribe(data => {
-      this._router.navigate(['/company/opening/create'])
-    }, error => {
-      console.log(error)
-    })
-  }
+	login(data: Object){
+		this.http.post('http://54.183.64.109/company/signin', data,{withCredentials: true })
+			.subscribe(
+				(data: any) => {
+					console.log(data)
+					this._router.navigate(['/company/opening/create'])
+				}, (error: HttpErrorResponse) => {
+					console.log(error)
+				})
+	}
 
-  login(data: Object){
-  	this.http.post('http://54.183.64.109/company/signin', data,{withCredentials: true })
-    .subscribe(data => {
-      this._router.navigate(['/company/opening/create']),
-      console.log(data)
-    }, error => {
-      console.log(error)
-    })
-  }
-
-  logout(){
-  	var data = {}
-  	return this.http.post('http://54.183.64.109/company/logout', data)
-  }
+	logout(){
+		return this.http.post('http://54.183.64.109/company/logout', {})
+	}
 }
--- cilent/src/app/common/services/user.service.ts
+++ cilent/src/app/common/services/user.service.ts
@@ -2,6 +2,7 @@ import { Router } from '@angular/router';
 import { Observable, Subject } from 'rxjs/Rx';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+const BASE_URL = 'http://54.183.64.109';
 
 @Injectable()
 export class UserService {
@@ -14,69 +15,80 @@ export class UserService {
   profile: any;
   setupStep = new Subject();
 
-  login(login_cred: Object){
-  	this.http.post('http://54.183.64.109/applicant/signin', login_cred)
-  	.subscribe(
-  		(data: any) => {
-        localStorage.setItem('user', JSON.stringify(data));
-        this.router.navigate(['jobs']);
-      },
-  		(err: HttpErrorResponse) => {
-        if (err["statusCode"] == "400") {
-          alert(err["message"])
-        }
-      }
-  	)
-  }
+	login(login_cred: Object){
+		const url = `${BASE_URL}/applicant/signin`;
+		this.http.post(url, login_cred)
+			.subscribe(
+				(data: any) => {
+					localStorage.setItem('user', JSON.stringify(data));
+					this.router.navigate(['jobs']);
+				},
+				(err: HttpErrorResponse) => {
+					if (err["statusCode"] == "400") {
+						alert(err["message"])
+					}
+				}
+			)
+	}
 
-  updateProfile(update_cred: Object){
-  	this.http.post('http://54.183.64.109/applicant/signin', update_cred)
-  	.subscribe(
-  		(data: any) => {
-        localStorage.setItem('user', JSON.stringify(data));
-        this.router.navigate(['jobs']);
-      },
-  		(err: HttpErrorResponse) => {
-        if (err["statusCode"] == "400") {
-          alert(err["message"])
-        }
-      }
-  	)
-  }
+	updateProfile(update_cred: Object){
+		const url = `${BASE_URL}/applicant/signin`;
+		this.http.post(url, update_cred)
+			.subscribe(
+				(data: any) => {
+				localStorage.setItem('user', JSON.stringify(data));
+				this.router.navigate(['jobs']);
+				},
+				(err: HttpErrorResponse) => {
+					console.log(err);
+					if (err.status === 400) {
+						console.log(err.message)
+					}
+				}
+			)
+	}
 
-  register(registration_cred: Object) {
-  	this.http.post('http://54.183.64.109/applicant/register', registration_cred)
-  	.subscribe(
-  		(data: any) => {
-        localStorage.setItem('user', JSON.stringify(data));
-        this.router.navigate(['setup']);
-      },
-  		(err: HttpErrorResponse) => {
-        console.log(err);
-        if (err["statusCode"] == "400"){
-          alert(err["message"])
-        }
-      }
-  	)
-  }
+	register(registration_cred: Object) {
+		const url = `${BASE_URL}/applicant/register`;
+		this.http.post(url, registration_cred)
+			.subscribe(
+				(data: any) => {
+					localStorage.setItem('user', JSON.stringify(data));
+					this.router.navigate(['setup']);
+				},
+				(err: HttpErrorResponse) => {
+					console.log(err);
+					if (err["statusCode"] == "400"){
+						alert(err["message"])
+					}
+				}
+			)
+	}
 
-  logout() {
-    return this.http.post('/applicant/register', {});
-  }
+	logout() {
+		const url = `${BASE_URL}/applicant/logout`;
+		return this.http.post(url, {});
+	}
 
-  getSession(){
-    this.http.get('http://54.183.64.109/applicant/activeSession')
-    .subscribe(data => console.log(data))
-  }
+	getSession(){
+		const url = `${BASE_URL}/applicant/activeSession`;
+		this.http.get(url)
+			.subscribe(
+				(data: any) => console.log(data),
+				(err: HttpErrorResponse) => console.log(err)
+				)
+	}
 
-  getProfile(user: String) {
-    this.http.get("/profile/" + user)
-    .subscribe(
-      data => this.profile = data
-    )
-  }
+	getProfile(id: String) {
+		const url = `${BASE_URL}/profile/${id}`;
+		this.http.get(url)
+			.subscribe(
+				(data: any) => this.profile = data,
+				(err: HttpErrorResponse) => console.log(err)
+			)
+	}
 
-sendProfileInfo(data: Object) {
+	sendProfileInfo(data: Object) {
 		const num = Number(localStorage.getItem('setupStep')) + 1;
 		localStorage.setItem('setupStep', num.toString());
 		this.setupStep.next(num);
@@ -84,17 +96,17 @@ sendProfileInfo(data: Object) {
 		const obj = stor !== '' ? JSON.parse(stor) : {};
 		const updatedObj = Object.assign(obj, data);
 		localStorage.setItem('profile', JSON.stringify(updatedObj));
-    if (num === 4) {
-      this.router.navigate(['jobs']);
-      this.register(updatedObj);
-    }
-  }
+		if (num === 4) {
+			this.register(updatedObj);
+			this.router.navigate(['jobs']);
+		}
+	}
   
-  stepBack() {
-    const num = Number(localStorage.getItem('setupStep')) - 1;
-    localStorage.setItem('setupStep', num.toString())
-    this.setupStep.next(num);
-  }
+	stepBack() {
+		const num = Number(localStorage.getItem('setupStep')) - 1;
+		localStorage.setItem('setupStep', num.toString())
+		this.setupStep.next(num);
+	}
 
 	getSetupStep(): Observable<any> {
 		return this.setupStep;
